705ae0e94a7132c51913879380d197235e60748f
stellamarma <stellamarmat@gmail.com>
2024-10-07 17:19:23 +0300

    Change the HTML using javascript

---
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,18 @@
+var text1= document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
+console.log(text1)
+    text1.innerHTML="stella";
+
+var tclass= document.getElementsByClassName("list")[0].style.color="green";
+
+var sele=document.querySelector("h1");
+
+sele.innerHTML="Good bey";
+var cele=document.querySelector("li a").style.color="red";
+
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.body.classList.toggle("invisible");
+
+document.body.classList.toggle("invisible");
+
+document.querySelector("a").setAttribute("href","https://gr.pinterest.com/");
