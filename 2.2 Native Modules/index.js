3606005fb979c71201347111f428d3177a4ed26c
stellamarma <stellamarmat@gmail.com>
2024-10-12 10:42:14 +0300

    Create file using node.js

---
+++ 2.2 Native Modules/index.js
@@ -0,0 +1,14 @@
+const fs=require("fs");
+
+// fs.writeFile("message.txt","Hello from trilofos xd", (err) => {
+//     if (err) throw err;
+//     console.log("The file has been saved!");
+//   });
+
+
+
+fs.readFile('./message.txt',"utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
