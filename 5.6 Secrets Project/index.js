e558c5b84dcc6c9ee7cbe740b51cd0f40a0b0c98
stellamarma <stellamarmat@gmail.com>
2024-10-19 19:20:26 +0300

    Watch others secrete

--- 5.6 Secrets Project/index.js
+++ 5.6 Secrets Project/index.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 app.get("/", async (req, res) => {
     try {
       const result = await axios.get("https://secrets-api.appbrewery.com/random");
-      res.render("index.ejs", {secret: result.data.secret,user: result.data.username });
+      res.render("index.ejs", {secret:result.data.secret,user: result.data.username });
     } catch (error) {
       console.log(error.response.data);
       res.status(500);
