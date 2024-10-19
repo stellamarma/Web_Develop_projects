9960266beb9529c4fe4ec20ad4fd7c575987213b
stellamarma <stellamarmat@gmail.com>
2024-10-19 18:39:29 +0300

    Create Rest API

--- 5.5 REST APIs/index.js
+++ 5.5 REST APIs/index.js
@@ -12,7 +12,7 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // https://secrets-api.appbrewery.com/
 
 //TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "";
+const yourBearerToken = "";//this is importan 
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
