a7800185ae7be305c68f48000a9fd5543a241aff
stellamarma <stellamarmat@gmail.com>
2024-11-15 18:28:41 +0200

    Add some env

--- 9.4+Environment+Variables/index.js
+++ 9.4+Environment+Variables/index.js
@@ -26,10 +26,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "pass",
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
   port: 5432,
 });
 db.connect();
